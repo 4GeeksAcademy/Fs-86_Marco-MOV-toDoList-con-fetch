bac2810ab249a4137bba64323bfe76cf5b91b137
M.V <marco.mov98@gmail.com>
2025-01-09 20:12:30 +0000

    First commit?

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+  const [state, setState] = useState([]); 
+  const [input, setInput] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
+  const getTodoList = async () => {
+    try {
+      const response = await fetch(`https://playground.4geeks.com/todo/users/marco_ortiz`, {
+        method: "GET",
+      });
+      const { todos } = await response.json();
+      setState(todos);
+    } catch (error) {
+      console.error("Error al obtener la lista de tareas:", error);
+    }
+  };
+
+  const addTodoItem = async () => {
+    if (!input.trim()) return;
+
+    try {
+      await fetch(`https://playground.4geeks.com/todo/todos/marco_ortiz`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ label: input, is_done: false }),
+      });
+      await getTodoList();
+      setInput("");
+    } catch (error) {
+      console.error("Error al agregar la tarea:", error);
+    }
+  };
+
+  const deleteTodoItem = async (todoId) => {
+    try {
+      await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+        method: "DELETE",
+      });
+      await getTodoList();
+    } catch (error) {
+      console.error("Error al eliminar la tarea:", error);
+    }
+  };
+
+  return (
+    <>
+		<div className="body">
+			<h1>To Do List</h1>
+			<br />
+			<div className="list-container">
+				<div className="header-container">
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						placeholder="Agregar nueva tarea"
+					/>
+					<br />
+					<button onClick={addTodoItem}>Agregar</button>
+				</div>
+				<br />
+				<br />
+				<ul>
+					{state.map((item, index) => (
+					<li key={index}>
+						{item.label}{" "}
+						<button className="delete-button" onClick={() => deleteTodoItem(item.id)}>x</button>
+					</li>
+					))}
+				</ul>
+				<p>{state.length} Items to do</p>
+			</div>
 		</div>
-	);
-};
+    </>
+  );
+}
 
-export default Home;
+export default App;
